d09e11f52aac6ce47621a3d3a778b484b42d7153
Ivan Hontarenko <null@IH-MBP13.local>
2020-09-02 10:20:19 +0300

    Rename WebAdmin

--- chat-app/src/main/java/io/manjo/chat/controller/KeepAliveController.java
+++
@@ -1,4 +0,0 @@
-package io.manjo.chat.controller;
-
-public class KeepAliveController {
-}
--- chat-app/src/main/java/io/manjo/chat/property/ApplicationProperties.java
+++
@@ -1,8 +0,0 @@
-package io.manjo.chat.property;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "application")
-public class ApplicationProperties {
-
-}
